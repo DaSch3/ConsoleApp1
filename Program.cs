45c26b98527f41756cfe49fccb5e675aa319afd0
David Schmitz <d.schmitz@dasch3.de>
2021-08-29 07:24:18 +0200

    Revert "Test 1"
    This reverts commit d4f7cd55ab2bb55677f192b11f71c68fc48e0f4e.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Wie geht es Dir?");
+            Console.WriteLine("Test Test Test Test Test Test");
+            Console.WriteLine("Zeile 3");
+            Console.WriteLine("Das ist ein Test.");
+            Console.WriteLine("Zeile 5");
+            Console.WriteLine("Noch ein Test.");
+            Console.WriteLine("Zeile 7");
         }
     }
 }
