65c6a7e8a728c3c53e5d99bee2aa813ee97df56b
David Schmitz <d.schmitz@dasch3.de>
2021-08-29 08:35:33 +0200

    Endgültig E N D E !!!

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Zeile 5");
             Console.WriteLine("Noch ein Test.");
             Console.WriteLine("Endlich fertig.");
-
+            Console.WriteLine();
             Console.WriteLine("Hello World! Wie geht es Dir?");
             Console.WriteLine("Test Test Test Test Test Test");
             Console.WriteLine("Zeile 3");
@@ -21,8 +21,11 @@ namespace ConsoleApp1
             Console.WriteLine("Zeile 5");
             Console.WriteLine("Noch ein Test.");
             Console.WriteLine("Endlich fertig.");
-
+            Console.WriteLine();
             Console.WriteLine("E N D E !");
+            Console.WriteLine();
+            Console.WriteLine("Bitte schlag auf die Tastatur, um fortzufahren!!!");
+            Console.ReadKey();
         }
     }
 }
