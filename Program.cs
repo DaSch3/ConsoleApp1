a4de88d5e5ef8f63ad51359cc5a546c3fece62e8
David Schmitz <d.schmitz@dasch3.de>
2021-08-29 06:06:56 +0200

    Noch ein Tst

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
             Console.WriteLine("Test Test Test Test Test Test");
             Console.WriteLine("Zeile 3");
             Console.WriteLine("Das ist ein Test.");
+            Console.WriteLine("Zeile 5");
+            Console.WriteLine("Noch ein Test.");
         }
     }
 }
