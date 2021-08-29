b101baa433c009f88eb545c432fa0b93a45a8385
David Schmitz <d.schmitz@dasch3.de>
2021-08-29 05:59:19 +0200

    Zeile 3 hinzugefügt

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Wie geht es Dir?");
+            Console.WriteLine("Test Test Test Test Test Test");
+            Console.WriteLine("Zeile 3");
         }
     }
 }
